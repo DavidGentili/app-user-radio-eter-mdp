67e1c0d0bb957df49eb06e29d6209518957fbd48
DavidGentili <DavidGenntili@gmail.com>
2022-07-18 11:53:29 -0300

    correction in grid dimensions of main section in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Header from '../components/header/Header'
 import publicity1 from '../../assets/publicity-1.gif'
 import publicity2 from '../../assets/publicity-2.gif'
 import MainSlider from '../components/mainSlider/MainSlider'
+import { getOficialPublicities } from '../services/publicity'
+
 
 import './home.css'
 
@@ -23,6 +25,9 @@ const Home = () => {
 
     const [publicities, setPublicities] = useState(initialPublicities)
 
+    useEffect( () => {
+        getOficialPublicities().then(data => console.log(data))
+    }, [])
 
     return (
         <main className='homeMain'>
@@ -31,8 +36,8 @@ const Home = () => {
                 
                 <MainSlider />
 
-                <div className="publicityImage boxContainer"><img className='publicityImage' src={publicities[0].src} alt="Esta es una publicidad" /></div>
-                <div className="publicityImage boxContainer"><img className='publicityImage' src={publicities[1].src} alt="Esta es una publicidad" /></div>
+                <div className="publicityImage boxContainer"><a href="#"><img className='publicityImage' src={publicities[0].src} alt="Esta es una publicidad" /></a></div>
+                <div className="publicityImage boxContainer"><a href="#"><img className='publicityImage' src={publicities[1].src} alt="Esta es una publicidad" /></a></div>
 
             </section>
         </main>
