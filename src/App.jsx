7dc6cf0b99a654073c93f937effc06dead83d8b6
DavidGentili <DavidGenntili@gmail.com>
2023-03-16 15:24:56 -0300

    correccion de import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from '@pages/home';
-import Player from '@components/generals/player';
+import Player from '@components/generals/Player';
 
 import Header from '@components/generals/Header'
 import Footer from '@components/generals/Footer'
