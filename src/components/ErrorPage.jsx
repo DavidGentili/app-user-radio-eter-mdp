fb902d49cc4ebc3eb4f20014eb0d191afae8cd66
DavidGentili <DavidGenntili@gmail.com>
2023-03-20 16:30:06 -0300

    Se corrigio redireccion

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { NotFoundIcon } from './Icons'
 
 export default function ErrorPage() {
     return (
-        <section className='errorPage'>ErrorPage</section>
+        <section className='errorPage'>
+            <div className="image">
+                <NotFoundIcon />
+            </div>
+            <h2>Opps, no se encontro el contenido que buscaba.</h2>
+            <Link to='/'>Volver al inicio</Link>
+        </section>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import App from './App'
 //Styles General
 import '@styles/general/globals.css'
 import '@styles/general/App.css'
+import '@styles/general/errorPage.css'
 import '@styles/general/footer.css'
 import '@styles/general/header.css'
 import '@styles/general/weatherPanel.css'
