5e036fa6e722e30cc98221308bf27dc274973bef
David Gentili <davidgenntili@gmail.com>
2023-05-28 19:34:44 -0300

    Se lee url API dese env

--- src/services/config.js
+++ src/services/config.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 // export const urlBase = 'https://api-radio-eter-mdp.herokuapp.com';
 // const urlBase = 'https://api-radio-eter-mdp.onrender.com'
-const urlBase = 'http://localhost:9000';
+const urlBase = import.meta.env.VITE_API_URL || 'http://localhost:9000';
 
 export const instance = axios.create({
     baseURL: urlBase,
 });
+
+console.log(urlBase)
 
 export const mediaPlayerUrl = 'https://videostream.shockmedia.com.ar:19360/radioetermdp/radioetermdp.m3u8';
 // export const mediaPlayerUrl = 'https://59537faa0729a.streamlock.net:443/radioetermdp/radioetermdp/playlist.m3u8';
