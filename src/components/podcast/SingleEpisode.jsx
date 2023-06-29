c9496d68b0c76ea3db54297d79d5623cc090dd8f
David Gentili <davidgenntili@gmail.com>
2023-06-29 16:13:34 -0300

    fix(Podcast) import

--- src/components/podcast/SingleEpisode.jsx
+++ src/components/podcast/SingleEpisode.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import UrlsPanel from '../generals/UrlsPanel'
+import { Link } from 'react-router-dom'
 
 export default function SingleEpisode({ title, podcastTitle, podcastId, imgUrl, urls }) {
 
     return (
         <article className='singleEpisode' style={{ backgroundImage: `url(${imgUrl})` }}>
-            <div className="info">
+            <Link to={`/podcast/${podcastId}`} className="info">
                 <h4>{title}</h4>
-                <a href={`/podcast/${podcastId}`}>{podcastTitle}</a>
-            </div>
+                <Link to={`/podcast/${podcastId}`}>{podcastTitle}</Link>
+            </Link>
             <UrlsPanel {...urls} />
             <div className='filter'></div>
         </article>
