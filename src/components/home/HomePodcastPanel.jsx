fc7b9e7f535d6520622d554839ea8044ad301c78
David Gentili <davidgenntili@gmail.com>
2023-06-29 16:04:03 -0300

    fix(podcast) link podcast in home

--- src/components/home/HomePodcastPanel.jsx
+++ src/components/home/HomePodcastPanel.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function HomePodcastPanel({ podcasts }) {
     return (
         <article className='podcastList'>
             <h3>Podcast</h3>
             {
-                podcasts.map(podcast => <div key={podcast.id} className='podcastItem' style={{ backgroundImage: `url(${podcast.imgUrl})` }}>
-                    <a href={`/podcast/${podcast.id}`} className="info">
+                podcasts.map(podcast => <Link to={`/podcast/${podcast.id}`} key={podcast.id} className='podcastItem' style={{ backgroundImage: `url(${podcast.imgUrl})` }}>
+                    <Link to={`/podcast/${podcast.id}`} className="info">
                         <h5>{podcast.title}</h5>
                         <p>{podcast.description}</p>
-                    </a>
+                    </Link>
                     <div className="filter"></div>
-                </div>)
+                </Link>)
             }
         </article>
     )
