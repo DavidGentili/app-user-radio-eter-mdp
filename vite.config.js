fca01b9b0bbcfb2349981d4282fd4a338cfb3afe
David Gentili <davidgenntili@gmail.com>
2023-11-29 12:15:22 -0300

    fix: cambio de configuracion para levantar vairable de entorno

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve : {
-    alias : {
-      '@components' : '/src/components',
-      '@helpers' : '/src/helpers',
-      '@pages' : '/src/pages',
-      '@styles' : '/src/styles',
-      '@services' : '/src/services',
-      '@assets' : '/assets',
-      '@hooks' : '/src/hooks',
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@helpers': '/src/helpers',
+      '@pages': '/src/pages',
+      '@styles': '/src/styles',
+      '@services': '/src/services',
+      '@assets': '/assets',
+      '@hooks': '/src/hooks',
 
     }
+  },
+  define: {
+    "process.env": process.env,
+    VITE_API_URL: process.env.VITE_API_URL
   }
 })
